4bde06517a1f47ca9d74d52e61ff9c4cfb23359f
Jadelet <slaethe@gmail.com>
2018-09-13 23:59:33 -0400

    completed bulk of HTML and basic framework of pseudocode. Some CSS repair needed, image is vertically stretching.

---
+++ Assets/Game.js
@@ -0,0 +1,61 @@
+//create var array of 10 question objects
+var superHeroArray = [
+"Ant-Man",  "Aquaman", "The Atom", "The Avengers", "Batgirl", "Batman", "Batwoman", "Black Canary", "Black Panther", "Captain America", "Catwoman", "Conan the Barbarian", "Daredevil", "The Defenders", "Doc Savage", "Doctor Strange", "Elektra", "Fantastic Four", "Ghost Rider", "Green Arrow", "Green Lantern", "Guardians of the Galaxy", "Hawkeye", "Hellboy", "Incredible Hulk", "Iron Fist", "Iron Man", "Marvelman", "Robin", "The Rocketeer", "The Shadow", "Spider-Man", "Sub-Mariner", "Supergirl", "Superman", "Teenage Mutant Ninja Turtles", "The Wasp", "Watchmen", "Wolverine", "Wonder Woman", "X-Men", "Zatanna", "Zatara"
+];
+
+
+// var to keep score
+var heroPoints = 0;
+
+// var to keep track of remaining guesses
+var guessRem = 7;
+
+//create function to decrease "guessRem" by 1 every time a wrong guess is made
+
+
+
+//var to hold html/dom elements for reference
+
+
+
+//code that assigns a random word by index in the array and returns as a string
+function retreiveWord(superHeroArray) {
+
+ 
+}
+//code to split the word selected from array into an array (need to figure out how to represent "Hello" below as randomly chosen array value, probably using document.getElementById):
+var retrieveWord = superHeroArray[arrayIndex]
+
+var txt = [];       // String
+txt.split("");           // Split in characters
+//reset game funcion
+function resetGame() {
+  // reset game variables
+  heroPoints = 0
+  guessRem = 7
+
+}
+
+//print score function
+function printScore(currentScore, questionsAsked){}
+// take arguments and print them to page
+scoreHTML.textContetn = currentScore + "out of" + qunsAsked + "correct!";
+//onkeyup event definition
+
+
+//code that checks letter chosen against each array value and if that value is === then prints that letter to its respective index.
+var tfAnswer = event.key;
+
+if tfAnswer !== "t" && tfAnswer !== "f" [
+  alert(the world of superheroes is black and white, there is no middle ground. Choose True or False.)
+]
+ if [q]===[a] && tfAnswer===[true]  {
+      alert{"You are correct, You're an aweseome superhero geek"}
+
+     if [q]!===[a] && tfAnswer====[false] {
+     alert{"You are correct, You're an aweseome superhero geek"}
+
+ 
+
+//randomly choose array value
+ 
